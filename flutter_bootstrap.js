ac9a531b41350a233c1eedfd0d2c054b30b2d4e6
Francis Kitema Nzoka <fkdevelopers254@gmail.com>
2026-08-18 11:13:09 -0400

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "756617124" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3784285132" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
